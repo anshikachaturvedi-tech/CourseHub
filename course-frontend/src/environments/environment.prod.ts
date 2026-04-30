1317728585995530792bbb00dd98ba038e7b1e54
anshikachaturvedi-tech <153982066+anshikachaturvedi-tech@users.noreply.github.com>
2026-04-30 07:05:21 -0400

    Update environment.prod.ts

--- course-frontend/src/environments/environment.prod.ts
+++ course-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://coursehub-production-cec2.up.railway.app/api'
+  apiUrl: 'https://coursehub-production-cec2.up.railway.app/api',
+  apiOrigin: 'https://coursehub-production-cec2.up.railway.app'
 };
